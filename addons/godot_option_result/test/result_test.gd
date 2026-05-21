8601808a10704f24919f67230f3c9f8b870f478c
Brian Rodriguez <brian@brianrodri.com>
2026-05-21 10:34:33 -0400

    test: fix callback verification guard

--- addons/godot_option_result/test/result_test.gd
+++ addons/godot_option_result/test/result_test.gd
@@ -201,7 +201,7 @@ func test_pipe(
 	var cb := mock(Callbacks) as Callbacks
 	var returned = input.pipe(cb.transform)
 	assert_that(returned).is_equal(input)
-	verify(cb, times_called).transform(input._value if times_called > 1 else any())
+	verify(cb, times_called).transform(input._value if times_called > 0 else any())
 
 
 func test_pipe_err(
@@ -215,7 +215,7 @@ func test_pipe_err(
 	var cb := mock(Callbacks) as Callbacks
 	var returned = input.pipe_err(cb.transform)
 	assert_that(returned).is_equal(input)
-	verify(cb, times_called).transform(input._value if times_called > 1 else any())
+	verify(cb, times_called).transform(input._value if times_called > 0 else any())
 
 
 func test_unwrap_returns_value_when_ok():
@@ -264,7 +264,7 @@ func test_unwrap_or_call(
 	var cb := mock(Callbacks) as Callbacks
 	do_return(expected).on(cb).transform(any())
 	assert_that(input.unwrap_or_call(cb.transform)).is_equal(expected)
-	verify(cb, times_called).transform(input._value if times_called > 1 else any())
+	verify(cb, times_called).transform(input._value if times_called > 0 else any())
 
 
 func test_map(
@@ -313,7 +313,7 @@ func test_map_or_call(
 	var cb := mock(Callbacks) as Callbacks
 	do_return(expected).on(cb).transform(any())
 	assert_that(input.map_or_call(cb.transform, len)).is_equal(expected)
-	verify(cb, times_called).transform(input._value if times_called > 1 else any())
+	verify(cb, times_called).transform(input._value if times_called > 0 else any())
 
 
 func test_and_then(
@@ -343,7 +343,7 @@ func test_and_then_call(
 	var cb := mock(Callbacks) as Callbacks
 	do_return(expected).on(cb).transform(any())
 	assert_that(input.and_then_call(cb.transform)).is_equal(expected)
-	verify(cb, times_called).transform(input._value if times_called > 1 else any())
+	verify(cb, times_called).transform(input._value if times_called > 0 else any())
 
 
 func test_or_else(
@@ -374,7 +374,7 @@ func test_or_else_call(
 	var cb := mock(Callbacks) as Callbacks
 	do_return(default).on(cb).transform(any())
 	assert_that(input.or_else_call(cb.transform)).is_equal(expected)
-	verify(cb, times_called).transform(input._value if times_called > 1 else any())
+	verify(cb, times_called).transform(input._value if times_called > 0 else any())
 
 
 func test_ok(
@@ -506,7 +506,7 @@ func test_map_member_or_call(
 	var cb := mock(Callbacks) as Callbacks
 	do_return(default).on(cb).transform(any())
 	assert_that(input.map_member_or_call(cb.transform, member_name)).is_equal(expected)
-	verify(cb, times_called).transform(input._value if times_called > 1 else any())
+	verify(cb, times_called).transform(input._value if times_called > 0 else any())
 
 
 func test_map_method_call_or_call(
@@ -526,7 +526,7 @@ func test_map_method_call_or_call(
 	var cb := mock(Callbacks) as Callbacks
 	do_return(default).on(cb).transform(any())
 	assert_that(input.map_method_call_or_call.bindv(arguments).call(cb.transform, method_name)).is_equal(expected)
-	verify(cb, times_called).transform(input._value if times_called > 1 else any())
+	verify(cb, times_called).transform(input._value if times_called > 0 else any())
 
 
 func test_and_then_member(
